3912e68b91dfc948b0a8acb0c2fb3df993c05cb0
stimko68 <stimko68@users.noreply.github.com>
2014-06-19 11:50:42 -0600

    Update all.py
    Changed file handler to use with open() to ensure each file is closed cleanly.

--- all.py
+++ all.py
@@ -1,7 +1,5 @@
 # Creates exercise .py files for Learn Python the Hard Way
 
-for i in range(1,53):
-	f = open('ex' + str(i) + '.py', 'w+')
-	f.write ("# Learn Python the Hard Way - Exercise No. %s\n" % str(i))
-	if f.closed != True:
-		f.close()
+for i in range(1, 53):
+	with open('ex' + str(i) + '.py', 'w+') as f:
+		f.write ("# Learn Python the Hard Way - Exercise No. %s\n" % str(i))
